c5a7d661592ed91a4ed0de1d6b4335071c31d9fb
Austin Liu <austin.liu05@yahoo.com>
2025-02-19 23:27:05 -0500

    Using local storage for mobile

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,6 @@
 // Import Firebase SDK
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore, doc, getDoc, collection, addDoc, updateDoc, getDocs, deleteDoc} from "firebase/firestore";
 
 // Your Firebase config (replace with your actual config)
@@ -17,6 +17,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+    .then(() => console.log("Auth persistence set to localStorage"))
+    .catch((error) => console.error("Error setting persistence:", error));
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
